6808260b79d09725e85ea9fe2c6a32544e44e6fd
ProyectosArimany <davidsolisalarco@gmail.com>
2022-05-30 08:26:15 -0600

    version 0.1

--- lib/docs/widgets/Pantallas/buttom_navigation_bar.dart
+++ lib/docs/widgets/Pantallas/buttom_navigation_bar.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ButtomNavigationBarW extends StatelessWidget {
-  const ButtomNavigationBarW({Key? key}) : super(key: key);
+  const ButtomNavigationBarW(
+      {Key? key, required this.indexTab, required this.changeCurrentTab})
+      : super(key: key);
 
-  final indexTab = 0;
+  final int indexTab;
+
+  final void Function(int currentTab) changeCurrentTab;
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +19,9 @@ class ButtomNavigationBarW extends StatelessWidget {
       selectedLabelStyle: const TextStyle(fontSize: 12, color: Colors.white),
       selectedIconTheme: const IconThemeData(color: Colors.white, size: 18),
       unselectedItemColor: Colors.grey,
-      unselectedLabelStyle: const TextStyle(fontSize: 12, color: Colors.grey),
+      unselectedLabelStyle: const TextStyle(fontSize: 10, color: Colors.grey),
       unselectedIconTheme: const IconThemeData(color: Colors.grey, size: 18),
-      onTap: (int newIndex) {},
+      onTap: (int newIndex) => changeCurrentTab(newIndex),
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
           icon: Icon(Icons.lock),
