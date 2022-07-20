04be5f8e6843abe62dabdbd343e0e3f1a40a80b2
David Solis <davidsolisalarco@gmail.com>
2022-07-20 15:11:37 -0600

    version 0.1

--- lib/docs/models/example_model.dart
+++ lib/docs/models/example_model.dart
@@ -1,38 +1,26 @@
 class Example {
-  String name;
-  String description;
-  double value;
+  late String name;
+  late String description;
+  late double value;
 
-  Example({
-    required this.name,
-    required this.description,
-    required this.value,
-  });
-
-  factory Example.fromJson(Map<String, dynamic> json) => Example(
-        name: json["name"] ?? '',
-        description: json["description"] ?? '',
-        value: double.tryParse(json["value"]) ?? 0,
-      );
+  Example(Map<String, dynamic> json) {
+    name = json["name"] ?? '';
+    description = json["description"] ?? '';
+    value = double.tryParse(json["value"]) ?? 0;
+  }
 
   Map<String, dynamic> toJson() => {
         "name": name,
         "description": description,
         "value": value,
       };
-}
 
-class Examples {
-  late List<Example> items;
+  factory Example.fromJson(Map<String, dynamic> json) => Example(json);
 
-  Examples(List<dynamic> list) {
-    if (list.isEmpty) items = [];
-
-    items = list.map((item) => Example.fromJson(item)).toList();
+  static List<Example> fromJsonList(List<Map<String, dynamic>> list) {
+    if (list.isEmpty) list = [];
+    return list.map((item) => Example.fromJson(item)).toList();
   }
-
-  List<Map<String, dynamic>> toJson() =>
-      items.map((item) => item.toJson()).toList();
 
   static List<Map<String, dynamic>> toJsonExternalList(List<Example> list) =>
       list.map((item) => item.toJson()).toList();
